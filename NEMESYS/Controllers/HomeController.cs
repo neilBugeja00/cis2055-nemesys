082352f8720d966357efb460a586908f02964ec3
neilBugeja00 <neilbugeja2000@gmail.com>
2022-06-04 11:10:00 +0200

    create hall of fame page

--- NEMESYS/Controllers/HomeController.cs
+++ NEMESYS/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace NEMESYS.Controllers
             return View();
         }
 
+        public IActionResult HallOfFame()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
